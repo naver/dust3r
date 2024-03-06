39766dd315f330a8abb121c20005c017e6cd4fb9
Gašper Spagnolo <gasper.spagnolo@outlook.com>
2024-03-06 21:22:25 +0100

    Eliminate conda from nvidia container

--- docker/files/entrypoint.sh
+++ docker/files/entrypoint.sh
@@ -5,8 +5,4 @@ set -eux
 DEVICE=${DEVICE:-cuda}
 MODEL=${MODEL:-DUSt3R_ViTLarge_BaseDecoder_512_dpt.pth}
 
-# Initialize Conda to make the `conda` command available in this script
-. /opt/conda/etc/profile.d/conda.sh
-conda activate dust3r
-
 exec python3 demo.py --weights "checkpoints/$MODEL" --device "$DEVICE" --local_network "$@"
