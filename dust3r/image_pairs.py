6b133a501f5760e0f22f8af3fef05dc9e227e9ac
Yohann Cabon <yohann.cabon@naverlabs.com>
2024-07-11 17:21:27 +0200

    make_pairs, add more options

--- dust3r/image_pairs.py
+++ dust3r/image_pairs.py
@@ -15,12 +15,39 @@ def make_pairs(imgs, scene_graph='complete', prefilter=None, symmetrize=True):
             for j in range(i):
                 pairs.append((imgs[i], imgs[j]))
     elif scene_graph.startswith('swin'):
-        winsize = int(scene_graph.split('-')[1]) if '-' in scene_graph else 3
+        iscyclic = not scene_graph.endswith('noncyclic')
+        try:
+            winsize = int(scene_graph.split('-')[1])
+        except Exception as e:
+            winsize = 3
         pairsid = set()
         for i in range(len(imgs)):
-            for j in range(1, winsize+1):
-                idx = (i + j) % len(imgs)  # explicit loop closure
+            for j in range(1, winsize + 1):
+                idx = (i + j)
+                if iscyclic:
+                    idx = idx % len(imgs)  # explicit loop closure
+                if idx >= len(imgs):
+                    continue
                 pairsid.add((i, idx) if i < idx else (idx, i))
+        for i, j in pairsid:
+            pairs.append((imgs[i], imgs[j]))
+    elif scene_graph.startswith('logwin'):
+        iscyclic = not scene_graph.endswith('noncyclic')
+        try:
+            winsize = int(scene_graph.split('-')[1])
+        except Exception as e:
+            winsize = 3
+        offsets = [2**i for i in range(winsize)]
+        pairsid = set()
+        for i in range(len(imgs)):
+            ixs_l = [i - off for off in offsets]
+            ixs_r = [i + off for off in offsets]
+            for j in ixs_l + ixs_r:
+                if iscyclic:
+                    j = j % len(imgs)  # Explicit loop closure
+                if j < 0 or j >= len(imgs) or j == i:
+                    continue
+                pairsid.add((i, j) if i < j else (j, i))
         for i, j in pairsid:
             pairs.append((imgs[i], imgs[j]))
     elif scene_graph.startswith('oneref'):
@@ -52,13 +79,13 @@ def sel(x, kept):
 
 def _filter_edges_seq(edges, seq_dis_thr, cyclic=False):
     # number of images
-    n = max(max(e) for e in edges)+1
+    n = max(max(e) for e in edges) + 1
 
     kept = []
     for e, (i, j) in enumerate(edges):
-        dis = abs(i-j)
+        dis = abs(i - j)
         if cyclic:
-            dis = min(dis, abs(i+n-j), abs(i-n-j))
+            dis = min(dis, abs(i + n - j), abs(i - n - j))
         if dis <= seq_dis_thr:
             kept.append(e)
     return kept
