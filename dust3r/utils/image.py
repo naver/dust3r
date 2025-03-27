d98818874f4e626553d1c5696cb5038ee3d45be0
YaraElwakeel <yaraelwakeel010@gmail.com>
2025-03-27 22:12:35 +0100

    _

--- dust3r/utils/image.py
+++ dust3r/utils/image.py
@@ -103,59 +103,20 @@ def preprocess_ir_rgb(img_rgb,img_ir):
     return img_rgb,img_ir
 
 
-def load_images_test(folder_or_list, size, square_ok=False, verbose=True):
+
+def load_images_test(images, size, square_ok=False, verbose=True):
     """ open and convert all images in a list or folder to proper input format for DUSt3R
     """
-    if isinstance(folder_or_list, str):
-        if verbose:
-            print(f'>> Loading images from {folder_or_list}')
-        root, folder_content = folder_or_list, sorted(os.listdir(folder_or_list))
-
-    elif isinstance(folder_or_list, list):
-        if verbose:
-            print(f'>> Loading a list of {len(folder_or_list)} images')
-        root, folder_content = '', folder_or_list
-
-    else:
-        raise ValueError(f'bad {folder_or_list=} ({type(folder_or_list)})')
-
-    supported_images_extensions = ['.jpg', '.jpeg', '.png']
-    if heif_support_enabled:
-        supported_images_extensions += ['.heic', '.heif']
-    supported_images_extensions = tuple(supported_images_extensions)
-
     imgs = []
-    for path in folder_content:
-        if not path.lower().endswith(supported_images_extensions):
-            continue
-        img = exif_transpose(PIL.Image.open(os.path.join(root, path))).convert('RGB')
-        W1, H1 = img.size
-        if size == 224:
-            # resize short side to 224 (then crop)
-            img = _resize_pil_image(img, round(size * max(W1/H1, H1/W1)))
-        else:
-            # resize long side to 512
-            img = _resize_pil_image(img, size)
-        W, H = img.size
-        cx, cy = W//2, H//2
-        if size == 224:
-            half = min(cx, cy)
-            img = img.crop((cx-half, cy-half, cx+half, cy+half))
-        else:
-            halfw, halfh = ((2*cx)//16)*8, ((2*cy)//16)*8
-            if not (square_ok) and W == H:
-                halfh = 3*halfw/4
-            img = img.crop((cx-halfw, cy-halfh, cx+halfw, cy+halfh))
+    for img in images:
 
-        W2, H2 = img.size
-        if verbose:
-            print(f' - adding {path} with resolution {W1}x{H1} --> {W2}x{H2}')
-        imgs.append(dict(img=ImgNorm(img)[None], true_shape=np.int32(
-            [img.size[::-1]]), idx=len(imgs), instance=str(len(imgs))))
+        imgs.append({
+            'img': img.unsqueeze(0), 
+            'true_shape': np.int32([[224,224]]), 
+            'idx': 0, 
+            'instance': 0
+        })
 
-    assert imgs, 'no images foud at '+root
-    if verbose:
-        print(f' (Found {len(imgs)} images)')
     return imgs 
 
 def load_images(img, size, square_ok=False, verbose=False):
